2ed0b0939b41c74268d1e5cc161ac6c2c46da765
Jean-Michel Feurprier <jmfeurprier@gromweb.com>
2016-01-29 00:49:44 -0500

    wip

--- lib/perf/Form/HiddenInput.php
+++ lib/perf/Form/HiddenInput.php
@@ -10,4 +10,35 @@ class HiddenInput extends Field
 {
 
     const FIELD_TYPE_ID = 'input.hidden';
+
+    /**
+     *
+     *
+     * @var string
+     */
+    private $value;
+
+    /**
+     * Constructor.
+     *
+     * @param string $name
+     * @param string $value
+     * @return void
+     */
+    public function __construct($name, $value = '')
+    {
+        parent::__construct($name);
+
+        $this->value = $value;
+    }
+
+    /**
+     *
+     *
+     * @return mixed
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
 }
